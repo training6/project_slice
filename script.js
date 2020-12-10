5a371cc600a10a5b1c5b54bb8f5567d2cc15798a
Terez <69874794+training6@users.noreply.github.com>
2020-12-10 11:16:13 +0100

    code first half added

--- script.js
+++ script.js
@@ -4,36 +4,69 @@
 //van egy törlési lehetőség
 function pageLoaded() {
 
-//változó létrehozása, amibe írom a root-ot
+	//változó létrehozása, amibe írom a root-ot
+
+	let root = document.getElementById("root");
+
+	// változóba el kell tárolnunk, hogy mi lesz a html struktúrája
+	//hozzáadni root-hoz sorok számát (10)
+	//mező, checkbox, törlés gomb
+
+	let maxNumber = 10;
+
+	let row = `
+		<div class="row">
+			<input type="checkbox">
+			<input type="text">
+			<button type="button" class="delete">Delete</button>
+		</div>
+	`;
+		//console.log(row);
+
+	//for ciklus a sorok számára (10)
+
+	for (let i = 0; i < maxNumber; i++) {
+
+		root.insertAdjacentHTML("beforeend", row);
+	}
+
+	//root elemen kívülre gomb a végére (insertAdjacentHTML afterend),
+	//amivel kattintásra új elemet tudunk létrehozni
+
+	root.insertAdjacentHTML("afterend", `
+		<button id="add">Új sor hozzáadása</button>
+		`
+	);
 
 
+	//click esemény hozzáadása a gombhoz (addEventListener)
 
-// változóba el kell tárolnunk, hogy mi lesz a html struktúrája
+	function newItem() {
+		root.insertAdjacentHTML("beforeend",
+		row);
 
+		//TODO: rá kellene tenni az eseményfigyelőt az újonnan létrehozott sorokra is (66.sor)
+	}
 
+	document.getElementById("add").addEventListener("click", newItem);
 
-//for ciklus a sorok számára (10)
-//hozzáadni root-hoz
-//mező, checkbox, törlés
+	//kijelölöm class név alapján az összes törlés gombot és elmentem egy változóba
 
+	let removeBtns = root.querySelectorAll(".delete"); 
+	//console.log(removeBtns);
 
+	//for ciklus, click eseményt rá tudok adni mindegyik törlés gombra
 
-//lista végére vagy root elemen kívülre gomb a végére (insertAdjacentHTML afterend)
+	function removeItem(e) {
+		console.log(e.target);
+		e.target.parentElement.remove();
+	}
 
+	for (let i = 0; i < removeBtns.length; i++) {
+		removeBtns[i].addEventListener("click", removeItem);
+	}
 
-
-//click esemény hozzáadása a gombhoz (addEventListener)
-
-
-
-//kijelölöm class név alapján az összes törlés gombot és elmentem egy változóba
-
-
-
-//for ciklus, click eseményt rá tudok adni mindegyik törlés gombra
-
-
-//hogyan tudom megjelölni, hogy mit szeretnék kitörölni
+	//hogyan tudom megjelölni, hogy mit szeretnék kitörölni
 
 
 
@@ -41,4 +74,5 @@ function pageLoaded() {
 
 
 }
-	window.addEventListener("load", pageLoaded);
+
+window.addEventListener("load", pageLoaded);
