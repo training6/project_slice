171860a9168bd8b08470a695b9c496439a11b6e8
Terez <69874794+training6@users.noreply.github.com>
2020-12-10 09:31:19 +0100

    project scope_steps

---
+++ script.js
@@ -0,0 +1,44 @@
+//to do applikációt készítünk, alapértelmezésben 10 üres mező áll rendelkezésre, 
+//de hozzá tudunk adni újakat és minden mezőben van egy checkbox, 
+//van egy text input mező, ami a feladat leírását beleírjuk és 
+//van egy törlési lehetőség
+function pageLoaded() {
+
+//változó létrehozása, amibe írom a root-ot
+
+
+
+// változóba el kell tárolnunk, hogy mi lesz a html struktúrája
+
+
+
+//for ciklus a sorok számára (10)
+//hozzáadni root-hoz
+//mező, checkbox, törlés
+
+
+
+//lista végére vagy root elemen kívülre gomb a végére (insertAdjacentHTML afterend)
+
+
+
+//click esemény hozzáadása a gombhoz (addEventListener)
+
+
+
+//kijelölöm class név alapján az összes törlés gombot és elmentem egy változóba
+
+
+
+//for ciklus, click eseményt rá tudok adni mindegyik törlés gombra
+
+
+//hogyan tudom megjelölni, hogy mit szeretnék kitörölni
+
+
+
+
+
+
+}
+	window.addEventListener("load", pageLoaded);
